5eb3faf69329da1cdf65f2682af4399fac31203b
Tainá Maia <taina.cmaia@hotmail.com>
2022-11-16 19:21:53 -0300

    Chamando css pelo js

--- src/screens/category/categoryList/categoryList.js
+++ src/screens/category/categoryList/categoryList.js
@@ -49,11 +49,12 @@ main.appendChild(tableDiv)
 /* Botões e busca */
 const asideDiv = document.createElement('div');
 asideDiv.setAttribute('id', 'aside');
-const h3 = document.createElement('h3');
-h3.innerText = 'Buscar por palavra-chave:'
 const searchDiv = document.createElement('div');
+searchDiv.setAttribute('id', 'search');
 const editDiv = document.createElement('div');
+editDiv.setAttribute('id', 'edit');
 const input = document.createElement('input');
+input.setAttribute('placeholder', 'Buscar palavra-chave...')
 const btnSearch = document.createElement('button');
 btnSearch.textContent = 'Buscar';
 const btnEdit = document.createElement('button');
@@ -62,7 +63,6 @@ const btnDelete = document.createElement('button');
 btnDelete.textContent = 'Remover';
 
 // asideDiv.appendChild(select)
-searchDiv.appendChild(h3)
 searchDiv.appendChild(input)
 searchDiv.appendChild(btnSearch)
 asideDiv.appendChild(searchDiv)
@@ -73,51 +73,12 @@ main.appendChild(asideDiv)
 document.body.appendChild(main);
 
 
-/* Style */
-Object.assign(main.style, {
-    display: 'flex',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    margin: '10vw 15vw'
-    })
+/* Chamando o CSS */
+const head = document.getElementsByTagName('head')[0];
+const link = document.createElement('link');
 
-Object.assign(table.style, {
-    border: '10px solid black',
-    'font-size': '30px',
-    textAlign: 'center',
-    width: '35vw',
-    margin: '20px'
-    })
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = './categoryList.css';
 
-Object.assign(searchDiv.style, {
-    padding: '10px',
-    })
-
-Object.assign(editDiv.style, {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    alignContent: 'center',
-    padding: '10px',
-    })
-
-Object.assign(thead.style, {
-    'font-size': '30px',
-})
-
-document.querySelector('table').style.border = 'solid 5px black';
-document.querySelector('table').style.borderCollapse = 'collapse';
-let celulas = document.querySelectorAll('tr');
-for(let i = 0; i < celulas.length; i++){
-    celulas[i].style.border = 'solid 1px black';
-}
-celulas = document.querySelectorAll('td');
-for(let i = 0; i < celulas.length; i++){
-    celulas[i].style.border = 'solid 1px black';
-}
-
-const button = document.querySelectorAll('button');
-for(let i = 0; i < button.length; i++){
-    button[i].style.width = '70px';
-    button[i].style.margin = '10px';
-}
+head.appendChild(link);
