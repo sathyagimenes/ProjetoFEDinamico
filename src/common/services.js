66e09ddaa446f30f4bf6929bcea4e167568f0930
sathyagimenes <sathya.gimenes@gmail.com>
2022-11-18 15:17:03 -0300

    separei css e adicionei API de registro de categoria

--- src/common/services.js
+++ src/common/services.js
@@ -1,81 +1,107 @@
-const groupCode = '3513d2d8-d47e-4da4-a61e-0ed144dd1c7f';
-const baseURL = 'http://estabelecimentos.letscode.dev.netuno.org:25390/services/';
+const groupCode = "3513d2d8-d47e-4da4-a61e-0ed144dd1c7f";
+const baseURL =
+  "http://estabelecimentos.letscode.dev.netuno.org:25390/services/";
 
-async function GetCategories () {
+async function GetCategories() {
   const response = await fetch(`${baseURL}category/list`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       text: "",
       group: {
-        uid: groupCode
-      }
-    })
-  }).catch(error => {
-    console.log('Erro na comunicação:', error);
+        uid: groupCode,
+      },
+    }),
+  }).catch((error) => {
+    console.log("Erro na comunicação:", error);
   });
 
-  if(!response.ok){
+  if (!response.ok) {
     errorHandler(response);
-    return []; 
+    return [];
   }
-  
+
   return await response.json();
 }
 
-async function GetCompanies () {
+async function GetCompanies() {
   const response = await fetch(`${baseURL}establishment/list`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       text: "",
       group: {
-        uid: groupCode
-      }
-    })
-  }).catch(error => {
-    console.log('Erro na comunicação:', error);
+        uid: groupCode,
+      },
+    }),
+  }).catch((error) => {
+    console.log("Erro na comunicação:", error);
   });
 
-  if(!response.ok){
+  if (!response.ok) {
     errorHandler(response);
-    return []; 
+    return [];
   }
-  
+
   return await response.json();
 }
 
-async function GetCompaniesByCategory (categoryCode) {
+async function GetCompaniesByCategory(categoryCode) {
   const response = await fetch(`${baseURL}establishment/list`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       text: "",
       category: {
-        uid: categoryCode
+        uid: categoryCode,
       },
       group: {
-        uid: groupCode
-      }
-    })
-  }).catch(error => {
-    console.log('Erro na comunicação:', error);
+        uid: groupCode,
+      },
+    }),
+  }).catch((error) => {
+    console.log("Erro na comunicação:", error);
   });
 
-  if(!response.ok){
+  if (!response.ok) {
     errorHandler(response);
-    return []; 
+    return [];
   }
-  
+
   return await response.json();
 }
 
-function errorHandler(response){
-    console.log('Erro : ', response.status, ' - ', response.statusText);
+async function PostCategory({ id, name }) {
+  const response = await fetch(`${baseURL}category`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      code: id,
+      name: name,
+      group: {
+        uid: groupCode,
+      },
+    }),
+  }).catch((error) => {
+    console.log("Erro na comunicação:", error);
+  });
+
+  if (!response.ok) {
+    errorHandler(response);
+    return [];
+  }
+
+  return await response.json();
+}
+
+function errorHandler(response) {
+  console.log("Erro : ", response.status, " - ", response.statusText);
 }
