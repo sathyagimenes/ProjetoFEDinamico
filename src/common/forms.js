9f55ed10078a2811989cdca92b234292814b8607
sathyagimenes <sathya.gimenes@gmail.com>
2022-11-24 10:52:26 -0300

    troquei field para fieldset

--- src/common/forms.js
+++ src/common/forms.js
@@ -5,7 +5,7 @@ function createRows(rowName) {
 }
 
 function createFields({ fieldName, title, inputType = "text", inputName }) {
-  const field = CreateElementWithAttribute("field", "name", fieldName);
+  const field = CreateElementWithAttribute("fieldset", "name", fieldName);
   field.classList.add("field");
   const titleElement = CreateElementWithAttribute("h3", "name", "label");
   titleElement.textContent = title + ":";
